e7d3880bd7d02bf67d362d4f9e22e7073accfeac
Colin Rymer <colin.rymer@gmail.com>
2019-11-14 12:39:10 -0500

    update dep handling

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-  FROM node:current-alpine
+FROM node:current-alpine
 
-  RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ v0.9.13
-  RUN apk --update add jq git && \
-  rm -rf /var/lib/apt/lists/* && \
-  rm /var/cache/apk/*
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ v0.9.13
+RUN apk --update add jq git && \
+rm -rf /var/lib/apt/lists/* && \
+rm /var/cache/apk/*
 
-  COPY entrypoint.sh /entrypoint.sh
+COPY entrypoint.sh /entrypoint.sh
 
-  ENTRYPOINT ["/entrypoint.sh"]
+WORKDIR /npm
+
+COPY package.json yarn.lock /npm
+
+RUN yarn install
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,13 @@
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-npm install -g \
-  @rentpath/eslint-config-rentpath@3.0.1 \
-  eslint@5.12.1 \
-  eslint-import-resolver-alias@1.1.2 \
-  eslint-plugin-mocha@5.2.1 \
-  eslint-plugin-standard@4.0.1 \
-  tslint@5.20.0 \
-  tslint-import-group-ordering@2.1.1 \
-  tslint-lines-between-class-members@1.2.4 \
-  tslint-no-circular-imports@0.7.0
+LINT_BIN=/npm/node_modules/.bin
 
-eslint --version
-tslint --version
+$LINT_BIN/eslint --version
+$LINT_BIN/tslint --version
 
-eslint -f="stylish" "${INPUT_LINT_DIRS:-'.'}" \
+$LINT_BIN/eslint -f="stylish" "${INPUT_LINT_DIRS:-'.'}" \
     | reviewdog -f="eslint" -reporter=github-pr-check
 
-tslint -p . -c tslint.json \
+$LINT_BIN/tslint -p . -c tslint.json \
     | reviewdog -f="tslint" -reporter=github-pr-check
