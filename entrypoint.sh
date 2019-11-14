4151ce0c5d18128261f315e2c62dbea677cbb631
Colin Rymer <colin.rymer@gmail.com>
2019-11-14 12:54:15 -0500

    update dep handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 LINT_BIN=/npm/node_modules/.bin
+
+cd $GITHUB_WORKSPACE
 
 $LINT_BIN/eslint --version
 $LINT_BIN/tslint --version
