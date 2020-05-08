3ea49a1e96c1d0945f40d8ca77f18d9dd4f633e1
Tad Johnston <tjohnston@rentpath.com>
2020-05-08 11:33:04 -0700

    more

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 cd $GITHUB_WORKSPACE
 
+npm install -g eslint@$(jq -r '.dependencies.eslint // .devDependencies.eslint | gsub("[\\^\\~]"; "")' package.json)
+
 LINT_BIN=/npm/node_modules/.bin
 
 if [ "${INPUT_TSLINT}" == 'true' ]; then
@@ -25,11 +27,11 @@ else
 
   if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
     # Use jq and github-pr-review reporter to format result to include link to rule page.
-    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'}  --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" -f json \
+    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" -f json \
       | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
       | reviewdog -efm="%f:%l:%c:%m" -name="${INPUT_NAME}:-eslint}" -reporter=github-pr-review -level="${INPUT_LEVEL}"
   else
-    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'}  --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" \
+    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" \
       | reviewdog -f="eslint" -name="${INPUT_NAME:-eslint}" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
   fi
 fi
