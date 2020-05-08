d3ca36895f851393d147787abb957d685f5fab58
Tad Johnston <tjohnston@rentpath.com>
2020-05-08 11:37:30 -0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,30 +8,30 @@ cd $GITHUB_WORKSPACE
 
 npm install -g eslint@$(jq -r '.dependencies.eslint // .devDependencies.eslint | gsub("[\\^\\~]"; "")' package.json)
 
-LINT_BIN=/npm/node_modules/.bin
+NPM_BIN=$(npm bin)/eslint
 
 if [ "${INPUT_TSLINT}" == 'true' ]; then
-  $LINT_BIN/tslint --version
+  $NPM_BIN/tslint --version
 
   if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
     # Use jq and github-pr-review reporter to format result to include link to rule page.
-    $LINT_BIN/tslint --project ${INPUT_LINT_DIRS:-'.'} --config "${INPUTLINT_CONFIG:-tslint.json}" --format json \
+    $NPM_BIN/tslint --project ${INPUT_LINT_DIRS:-'.'} --config "${INPUTLINT_CONFIG:-tslint.json}" --format json \
       | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
       | reviewdog -efm="%f:%l:%c:%m" -name="${INPUT_NAME}:-tslint}" -reporter=github-pr-review -level="${INPUT_LEVEL}"
   else
-    $LINT_BIN/tslint --format="stylish" --project ${INPUT_LINT_DIRS:-'.'} --config "${INPUT_LINT_CONFIG:-tslint.json}" \
+    $NPM_BIN/tslint --format="stylish" --project ${INPUT_LINT_DIRS:-'.'} --config "${INPUT_LINT_CONFIG:-tslint.json}" \
       | reviewdog -f="tslint" -name="${INPUT_NAME:-tslint}" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
   fi
 else
-  $LINT_BIN/eslint --version
+  $NPM_BIN/eslint --version
 
   if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
     # Use jq and github-pr-review reporter to format result to include link to rule page.
-    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" -f json \
+    $NPM_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" -f json \
       | jq -r '.[] | {filePath: .filePath, messages: .messages[]} | "\(.filePath):\(.messages.line):\(.messages.column):\(.messages.message) [\(.messages.ruleId)](https://eslint.org/docs/rules/\(.messages.ruleId))"' \
       | reviewdog -efm="%f:%l:%c:%m" -name="${INPUT_NAME}:-eslint}" -reporter=github-pr-review -level="${INPUT_LEVEL}"
   else
-    $LINT_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" \
+    $NPM_BIN/eslint ${INPUT_LINT_DIRS:-'.'} --ext="${INPUT_LINT_EXT}" --config "${INPUT_LINT_CONFIG}" \
       | reviewdog -f="eslint" -name="${INPUT_NAME:-eslint}" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
   fi
 fi
